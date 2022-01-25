72d494e57d22e1ef1acca1053c6989679331b9fe
Joshua Cordoba <jcordobab101@icloud.com>
2022-01-25 16:42:01 -0600

    files udpate

--- hello.test.js
+++ hello.test.js
@@ -1,6 +1,6 @@
-var utils  = require('course-utilities');
+var utils = require('course-utilities');
 var hello = utils.load('./hello.js', 'hello');
 
 test('outputs the correct string', () => {
-  expect(hello()).toBe("Hello World!");
+    expect(hello()).toBe("Hello World");
 });
